01402e87475dbde13980fcbd1b6afc3ee798d9d0
toddmurphy <toddmurphy93@gmail.com>
2020-01-21 17:14:16 -0400

    set up knex file, connection info, installed dependencies

---
+++ knexfile.js
@@ -0,0 +1,13 @@
+// Update with your config settings.
+
+module.exports = {
+  development: {
+    client: 'postgres',
+    connection: {
+      host: 'localhost',
+      user: 'postgres',
+      password: 'postgres',
+      database: 'cars'
+    }
+  }
+};
