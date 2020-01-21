f858ed313ec32bac6f4546ae008b1129db67ba7d
toddmurphy <toddmurphy93@gmail.com>
2020-01-21 19:55:01 -0400

    working on put and delete

--- api/carsRouter.js
+++ api/carsRouter.js
@@ -36,4 +36,44 @@ router.post('/', async (req, res) => {
   }
 });
 
+//delete --> remove a car
+router.delete('/:id', async (req, res) => {
+  try {
+    await db('cars')
+      .where('id', req.params.id)
+      .del();
+
+    res.status(204).end();
+  } catch (error) {
+    res.status(500).json({ message: 'Sorry, no car deleted from the server', error });
+  }
+});
+
+//put --> update a car
+router.put('/:id', async (req, res) => {
+  try {
+    const updateCar = {
+      make: req.body.make,
+      model: req.body.model,
+      VIN: req.body.model,
+      mileage: req.body.mileage,
+      transmission: req.body.transmission,
+      status: req.body.status
+    };
+    //updates a car info
+    await db('cars')
+      .where('id', req.params.id)
+      .update(updateCar);
+
+    //response in postman
+    res.json(
+      await db('cars')
+        .where('id', req.params.id)
+        .first()
+    );
+  } catch (error) {
+    res.status(500).json({ message: 'Sorry, no car info updated on the server', error });
+  }
+});
+
 module.exports = router;
